fec4fe28fc5ff974992caeb8346d9cb76ab98902
Phumu <phumu.thoka@lexisnexis.co.za>
2026-04-17 11:04:40 +0200

    pthoka : initial commit

---
+++ src/main/java/com/twoguysoneai/customermeetingprepagent/CustomerMeetingPrepAgentApplication.java
@@ -0,0 +1,13 @@
+package com.twoguysoneai.customermeetingprepagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerMeetingPrepAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerMeetingPrepAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/twoguysoneai/customermeetingprepagent/CustomerMeetingPrepAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twoguysoneai.customermeetingprepagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerMeetingPrepAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
